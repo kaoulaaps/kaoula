1d5e36365b1000a2d03c7e621c1aca91adf6ecb4
lasse <77295879+lassv@users.noreply.github.com>
2022-08-15 09:57:48 +0200

    Update api endpoint

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,9 +3,22 @@ const router = require("express").Router();
 const Class = require("../database/models/Class")
 
 router.get("/classes",  async (req,res) => {
-    const classes = Class.find({})
-
-    res.send(classes)
+   
+        Class.find({}, (err, data) => {
+            const filteredData = data.map((d) => {
+                return {
+                    id: d.classId,
+                    name: d.name,
+                    description: d.description,
+                    teacher: d.teacher.uid,
+                    students: d.students.length,
+                    private: d.private,
+                    maxStudents: d.maxStudents,
+                };
+            });
+            res.send(filteredData);
+        });
+ 
 })
 
 module.exports = router;
