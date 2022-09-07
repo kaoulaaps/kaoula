1bdd17e349c2278d28461bf2103e1c858eb91574
lassev05 <vestergaardlasse2@gmail.com>
2022-09-07 20:52:01 +0200

    Servemon config

---
+++ servemon.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    projectName: "kaoula",
+    port: 4000, // Port to use
+    directory: "./public", // Directory to use
+    pkgManager: "servemon", // Package manager you want to use
+    watch: false,  // Watch for changes
+    open: false, // Open the browser
+    fileExplore: false, // Enable file explorer
+    logger: true, // Enable logger
+
+    
+}
+
+    
