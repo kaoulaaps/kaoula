f4d3fb2f931080df57d4b0a06791a581ee2f4686
lassev05 <vestergaardlasse2@gmail.com>
2022-09-12 17:31:24 +0200

    New description (not deployed yet)

--- kaoula.config.js
+++ kaoula.config.js
@@ -2,12 +2,7 @@ module.exports = {
     home: {
         site_name: "Kaoula",
         site_logo: "./kaoula.png",
-        site_description: `Dette er en skole platform der gør det nemt at oprette
-    klasser og oprette og administrere klasser. Du kan give
-    dine elever lektier for, samt oprette opgaver til dem.
-    Der er også muglighed for at skrive med dine elever i
-    grupper hvor det er dig der bestemmer hvem der kan
-    skrive/se.`,
+        site_description: `Få meget mere uden af skolen, med en skole portal fyldt med gode funktioner!`,
     },
 
     footer: {
